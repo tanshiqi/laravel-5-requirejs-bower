6152226e17a12f6ea0c90c4a2527428001d77094
atan <tanshiqi@gmail.com>
2015-04-16 12:18:12 +0800

    添加sample.blade.php

---
+++ resources/views/sample.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="csrf-token" content="{!! csrf_token() !!}">
+
+    <title>Blank Page</title>
+
+    <link rel="stylesheet" href="{!! env('REQ_CSS') !!}">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+
+
+    <script data-main="{!! env('REQ_JS') !!}" src="/js/require.js"></script>
+
+</body>
+
+</html>
